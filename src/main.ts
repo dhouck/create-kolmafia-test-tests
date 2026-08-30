d96a7272fc7e99e2b11d7f99fcbdbb0f7016d855
Daniel Houck <Software@DRHouck.me>
2026-08-30 19:05:13 -0400

    Fix lint error; add format errors

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,15 @@ import { myName, print } from "kolmafia";
 // Note: it is possible to call a script with multiple parameters, but usually the entire command
 // line is a string with one parameter.
 export function main(...args: string[]): void {
-	let greeting = `Welcome, ${myName()}!`;
-	print(greeting);
-	print("You have successfully built add-testing!");
-	if (args.length > 0) {
+	const greeting =
+	    `Welcome, ${myName()}!`
+	print ( greeting )
+	print ("You have successfully built add-testing!")
+	if(args.length > 0)
+	{
 		const argsString = JSON.stringify(args);
 		const argWord = args.length === 1 ? "argument" : "arguments";
 		print(`You called the script with ${args.length} ${argWord}: ${argsString}`);
 	}
+	;;;;;;;
 }
